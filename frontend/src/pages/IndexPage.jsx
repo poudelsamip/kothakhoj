d5faf472d2a4351d2f889356eb271ac02dbd32bc
Samip Poudel <144611884+poudelsamip@users.noreply.github.com>
2025-12-16 08:42:41 +0545

    Update IndexPage.jsx

--- frontend/src/pages/IndexPage.jsx
+++ frontend/src/pages/IndexPage.jsx
@@ -5,12 +5,15 @@ import axios from "axios";
 
 const IndexPage = () => {
   const [places, setPlaces] = useState([]);
+  const [loading, setLoading] = useState(true)
   useEffect(() => {
+    setLoading(true)
     axios.get("/places").then(({ data }) => {
       setPlaces(data);
+      setLoading(false)
     });
   }, []);
-  return <AllPlaces places={places} />;
+  return <AllPlaces places={places} loading={loading}/>;
 };
 
 export default IndexPage;
